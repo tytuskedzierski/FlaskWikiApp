83ce883033c7a75dc0a12ac189173644a078e19f
tytuskedzierski <tytuskedzierski@gmail.com>
2020-06-08 13:12:52 +0200

    main, error page

--- app.py
+++ app.py
@@ -20,11 +20,12 @@ def mainRoute():
         cursor.execute(sql_query)
         previousSearch = cursor.fetchall()
         connection.close()
-        return render_template('error.html', previous_search = previousSearch,
+        return render_template('main.html', previous_search = previousSearch,
                                              defaultLang = defaultLanguage,
-                                             app_adress = appAddress)
+                                             app_address = appAddress)
     except:
-        return('db connection error / sql error')
+        return render_template('error.html', error_message = 'db connection error / sql error',
+                                                  app_address = appAddress)
 
 @app.route('/Search/<string:lang>/<string:searchPhrase>')
 def wikiSearch(searchPhrase, lang):
@@ -36,7 +37,8 @@ def wikiSearch(searchPhrase, lang):
         previousSearch = cursor.fetchall()
         connection.close()
     except:
-        return('db connection error / sql error')
+        return render_template('error.html', error_message = 'db connection error / sql error',
+                                                  app_address = appAddress)
     try:
         wikipedia.set_lang(lang)
         return render_template('wikiSearch.html', searchlist = wikipedia.search(searchPhrase),
@@ -44,9 +46,10 @@ def wikiSearch(searchPhrase, lang):
                                                   search_phrase = searchPhrase,
                                                   defaultLang = defaultLanguage,
                                                   previous_search = previousSearch,
-                                                  app_adress = appAddress)
+                                                  app_address = appAddress)
     except:
-        return('wikipedia connection error')
+        return render_template('error.html', error_message = 'wikipedia connection error',
+                                                  app_address = appAddress)
 
 @app.route('/Show/<string:lang>/<string:searchPhrase>')
 def wikiShow(searchPhrase, lang):
@@ -61,7 +64,8 @@ def wikiShow(searchPhrase, lang):
             connection.commit()
             connection.close()
         except:
-            return('db connection error / sql error')
+            return render_template('error.html', error_message = 'db connection error / sql error',
+                                                  app_address = appAddress)
         
         return render_template('wikiShow.html',  pageTitle = page.title,
                                                  pageContent = page.content,
@@ -69,9 +73,10 @@ def wikiShow(searchPhrase, lang):
                                                  inLang=lang,
                                                  defaultLang = defaultLanguage,
                                                  searchlist = wikipedia.search(searchPhrase),
-                                                 app_adress = appAddress)
+                                                 app_address = appAddress)
     except:
-        return('wikipedia connection error / query error')
+        return render_template('error.html', error_message = 'wikipedia connection error / query error',
+                                                  app_address = appAddress)
 
 @app.errorhandler(404)
 def invalid_route(e):
@@ -82,11 +87,12 @@ def invalid_route(e):
         cursor.execute(sql_query)
         previousSearch = cursor.fetchall()
         connection.close()
-        return render_template('error.html', previous_search = previousSearch,
+        return render_template('main.html', previous_search = previousSearch,
                                              defaultLang = defaultLanguage,
-                                             app_adress = appAddress)
+                                             app_address = appAddress)
     except:
-        return('db connection error / sql error')
+        return render_template('error.html', error_message = 'db connection error / sql error',
+                                                  app_address = appAddress)
 
 
 if __name__ == "__main__":
